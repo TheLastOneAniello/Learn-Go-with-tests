a82276c7f45d18537930d11bf57551c452fddbd4
Don Dabbins <aniello.scartozzi@gmail.com>
2024-07-27 18:07:29 -0400

    for loops

--- repeat/repeat_test.go
+++ repeat/repeat_test.go
@@ -2,12 +2,20 @@ package iteration
 
 import "testing"
 
+const repeatCount = 5
+
 func Repeat(character string) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
-		repeated = repeated + character
+	for i := 0; i < repeatCount; i++ {
+		repeated += character
 	}
 	return repeated
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
 }
 
 func TestRepeat(t *testing.T) {
