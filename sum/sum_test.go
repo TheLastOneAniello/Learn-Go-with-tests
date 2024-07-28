99bfb8c032b45de5e26a8fd19329d70911ab836e
Don Dabbins <aniello.scartozzi@gmail.com>
2024-07-28 19:59:54 -0400

    sum refactor

---
+++ sum/sum_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func Sum(numbers [5]int) int {
+	sum := 0
+	for i := 0; i < 5; i++ {
+		sum += numbers[i]
+	}
+	return sum
+}
+
+func TestSum(t *testing.T) {
+
+	numbers := [5]int{1, 2, 3, 4, 5}
+
+	got := Sum(numbers)
+	want := 15
+
+	if got != want {
+		t.Errorf("got %d want %d given %v", got, want, numbers)
+
+	}
+}
