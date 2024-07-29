03e4df6f9cb7159e9d2052d34ed5c6c1142a4c3c
Don Dabbins <aniello.scartozzi@gmail.com>
2024-07-28 20:27:32 -0400

    slice refactor

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -2,23 +2,36 @@ package main
 
 import "testing"
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, numbers := range numbers {
+		sum += numbers
 	}
 	return sum
 }
 
 func TestSum(t *testing.T) {
+	t.Run("collections of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	numbers := [5]int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		want := 15
 
-	got := Sum(numbers)
-	want := 15
+		if got != want {
+			t.Errorf("got %d want %d given %v", got, want, numbers)
 
-	if got != want {
-		t.Errorf("got %d want %d given %v", got, want, numbers)
+		}
+	})
 
-	}
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d given %v", got, want, numbers)
+		}
+	})
+
 }
